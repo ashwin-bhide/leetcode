5b760383a8cb67379c7fbeb23ad2a2c90b22366d
ashwin-bhide <38453842+ashwin-bhide@users.noreply.github.com>
2020-03-03 15:02:49 -0600

    Create firstBadVersion.java

---
+++ firstBadVersion.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n==0)
+            return -1;
+        if(n==1)
+            return isBadVersion(1)?1:-1;
+        int start = 1, end = n, mid;
+        while(start<end){
+            mid = start+(end-start)/2;
+            if(isBadVersion(mid)){
+                end=mid;
+            } else
+                start = mid+1;
+        }
+        if(isBadVersion(start)) return start;
+        return -1;
+    }
+}

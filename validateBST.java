c71cb87e0f55519b9c1b78dad5f35eab6f0223f7
ashwin-bhide <38453842+ashwin-bhide@users.noreply.github.com>
2020-03-04 22:34:36 -0600

    Create validateBST.java

---
+++ validateBST.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root==null) return true;
+        Stack<TreeNode> s = new Stack<>();
+        double inOrder = -Double.MAX_VALUE; //Double.MIN_VALUE is non-negative, so to get least number we multiple MAX_VALUE by -1
+        //Inorder traversal using DFS(stacks) is best choice for BST problems
+        while(!s.isEmpty() || root!=null){
+            while(root!=null){
+                s.push(root);
+                root=root.left;
+            }
+        //Here, we compare with only one value(inorder) as all other numbers will be smaller than this due to ascending nature of inorder BST traversal
+            root = s.pop();
+            if(root.val<=inOrder)
+                return false;
+        
+            inOrder = root.val;
+            root=root.right;
+
+            
+        }
+        return true;
+    }
+}

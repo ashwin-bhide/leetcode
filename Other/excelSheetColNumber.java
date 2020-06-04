edf048a720d8d344972ad3e5e32cc8935283be36
ashwin-bhide <38453842+ashwin-bhide@users.noreply.github.com>
2020-06-04 17:46:04 -0500

    Create excelSheetColNumber.java

---
+++ Other/excelSheetColNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int titleToNumber(String s) {
+        if(s.length()==0)
+            return -1;
+        
+        int sum = 0;
+        for(char c:s.toCharArray()){
+            sum *= 26;
+            sum += c-'A'+1;
+        }
+        
+        return sum;
+    }
+}
